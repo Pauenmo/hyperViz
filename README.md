228b765a3e29e94290a5842aecf276b37c250803
PauTTM <70710906+PauTTM@users.noreply.github.com>
2021-04-16 14:22:30 +0100

    added histogram of number of disciplines

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Code for the SEMF hyperViz project (link coming soon)
 The initial goal is to parse data collected as a csv via registration forms and to visualize it in meaningful ways  
 
 # TODO:
-Basic histogram of how many people have selected what number of disciplines  
-Implement a random answer generator   
+Implement a random answer generator (with some sensible probabilities)  
 Test basic chord diagrams  
 
 # Already done
 Basic bar chart of how many times a discipline was mentioned in the form  
+Basic histogram of how many people have selected what number of disciplines  
--- hyperViz.py
+++ hyperViz.py
@@ -35,7 +35,7 @@ for discipline in discipline_count.keys():
         count += entry.count(discipline)
     discipline_count[discipline] = count
    
-print(discipline_count)
+print("discipline_count: ", discipline_count)
 
 #Finally, we draw this as a bar chart
 #These first two lines are just to force y labels to be integers
@@ -44,4 +44,33 @@ ax.yaxis.get_major_locator().set_params(integer=True)
 
 plt.bar(discipline_count.keys(), discipline_count.values())
 plt.xticks(rotation = 90)
+
+#Graph 2: basic bar chart of how many people selected each number of disciplines
+#Need to ignore whatever was entered in the 'Other' field
+#frequencies: list where every entry corresponds to an answer and every value to the number of disciplines in it
+frequencies = []
+for entry in data.loc[:,'Disciplines']:
+    entry = entry.split(";")
+    count = 0
+    for discipline in entry:
+        if discipline in discipline_count.keys():
+            count += 1
+    if count > 0:
+        frequencies.append(count)
+    
+print("frequencies: ", frequencies)
+
+#Finally, we plot this as a bar chart
+keys = list(set(frequencies))
+keys.sort()
+values = []
+for k in keys:
+    values.append(frequencies.count(k))
+
+#These first two lines are just to force y labels to be integers
+ax = plt.figure().gca()
+ax.yaxis.get_major_locator().set_params(integer=True)
+plt.bar(keys, values)
+
+
 
